9e2e07c957b14b636c70e6319eb059e17bdcaad9
Augusto Ribeiro <augusto.ribeiro@nappsolutions.com>
2024-11-07 11:30:46 -0300

    feat: Adiciona SpeedInsights da Vercel para métricas do website.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Augusto Ribeiro',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
