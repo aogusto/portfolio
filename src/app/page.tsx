0d00d0019bce1706f3d6813ac93f26ef85eae692
Augusto Ribeiro <augusto.ribeiro@nappsolutions.com>
2024-11-07 10:44:22 -0300

    feat: Adiciona prettier e eslint ao projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,52 @@
-'use client'
-import { motion, useMotionValue, useTransform, animate } from "framer-motion";
-import { useEffect, useState } from "react";
-import CursorBlinker from "@/app/components/CursorBlinker";
+'use client';
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import CursorBlinker from '@/app/components/CursorBlinker';
 
 export default function Home() {
-    const baseText = "Olá, meu nome é Augusto!" as string;
-    const initialText = "Olá, meu nome é Ausguto!" as string;
-    const count = useMotionValue(0);
-    const [isCorrecting, setIsCorrecting] = useState(false);
+  const baseText = 'Olá, meu nome é Augusto!' as string;
+  const initialText = 'Olá, meu nome é Ausguto!' as string;
+  const count = useMotionValue(0);
+  const [isCorrecting, setIsCorrecting] = useState(false);
 
-    const rounded = useTransform(count, (latest) => Math.round(latest));
-    const displayText = useTransform(rounded, (latest) =>
-        (isCorrecting ? baseText : initialText).slice(0, latest)
-    );
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    (isCorrecting ? baseText : initialText).slice(0, latest)
+  );
 
-    useEffect(() => {
-        const controlsStep1 = animate(count, initialText.length, {
-            type: "tween",
-            duration: 2,
-            ease: "easeInOut",
+  useEffect(() => {
+    const controlsStep1 = animate(count, initialText.length, {
+      type: 'tween',
+      duration: 2,
+      ease: 'easeInOut',
+      onComplete: () => {
+        setTimeout(() => {
+          animate(count, 17, {
+            type: 'tween',
+            duration: 0.5,
+            ease: 'easeInOut',
             onComplete: () => {
-                setTimeout(() => {
-                    animate(count, 17, {
-                        type: "tween",
-                        duration: 0.5,
-                        ease: "easeInOut",
-                        onComplete: () => {
-                            setIsCorrecting(true);
-                            animate(count, baseText.length, {
-                                type: "tween",
-                                duration: 1.5,
-                                ease: "easeInOut"
-                            });
-                        },
-                    });
-                }, 1200);
+              setIsCorrecting(true);
+              animate(count, baseText.length, {
+                type: 'tween',
+                duration: 1.5,
+                ease: 'easeInOut',
+              });
             },
-        });
+          });
+        }, 1200);
+      },
+    });
 
-        return () => controlsStep1.stop();
-    }, []);
+    return () => controlsStep1.stop();
+  }, []);
 
-    return (
-        <div className="w-full h-screen flex flex-col justify-start items-center p-8">
-            <h1 className="text-5xl font-bold">
-                <motion.span>{displayText}</motion.span>
-                <CursorBlinker />
-            </h1>
-        </div>
-    );
+  return (
+    <div className="w-full h-screen flex flex-col justify-start items-center p-8">
+      <h1 className="text-5xl font-bold">
+        <motion.span>{displayText}</motion.span>
+        <CursorBlinker />
+      </h1>
+    </div>
+  );
 }
